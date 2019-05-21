a2436f7a5094792d91687c1cdbba2b68d38fd016
zhengshuaiwei <zhengshuaiwei@le.com>
2019-05-21 16:03:39 +0800

    优化轮播图

--- CircleDemo/CircleDemo/ViewController.swift
+++ CircleDemo/CircleDemo/ViewController.swift
@@ -14,11 +14,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let circle = ZJCircleView(frame: CGRect(x: 0, y: 88, width: KSCREEN_WIDTH, height: 300))
+        circle.titles = ["lotus0","lotus1","lotus2","lotus3"]
         circle.setImageNames(images: ["lotus0","lotus1","lotus2","lotus3"])
+        circle.delegate = self
         self.view.addSubview(circle)
-        
-        
-    }
 
+    }
 }
 
+extension ViewController: ZJCircleViewDelegate {
+    func circleViewClick(clickAtIndex: Int) {
+        print("click index is \(clickAtIndex)")
+    }
+}
--- CircleDemo/CircleDemo/ZJCircleView.swift
+++ CircleDemo/CircleDemo/ZJCircleView.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
+
+public protocol ZJCircleViewDelegate: NSObjectProtocol {
+    func circleViewClick(clickAtIndex:Int);
+}
+
 class ZJCircleView: UIView {
     
     let scrollView: UIScrollView!
     let pageControl: UIPageControl!
+    var pageNo = 0// 0 1 2 3 ....
     var imageNames : Array<Any>
-
+    var titles =  [String]()
+    var timer: Timer?
+    
+    weak open var delegate: ZJCircleViewDelegate?
+    
+    
     override init(frame: CGRect) {
         pageControl = UIPageControl()
         scrollView = UIScrollView()
         imageNames = []
+  
         super.init(frame: frame)
-        
+
         scrollView.frame = self.bounds
         scrollView.backgroundColor = UIColor.blue
         scrollView.isPagingEnabled = true
@@ -28,26 +40,52 @@ class ZJCircleView: UIView {
         scrollView.bounces = false
         self.addSubview(scrollView)
         
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        scrollView.addGestureRecognizer(tapGesture)
+
+        timer = Timer(timeInterval: 1.5, target: self, selector: #selector(forTimer), userInfo: nil, repeats: true)
+        RunLoop.current.add(timer!, forMode: .default)
+
     }
-    
+    @objc func forTimer(timer:Timer){
+
+        var pageNo_ = pageNo
+        pageNo_ += 1
+        if pageNo_ == imageNames.count - 1 {
+            scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: false)
+            pageNo_ = 0
+        }
+        pageNo_ += 1
+        scrollView.setContentOffset(CGPoint(x:self.frameW * CGFloat(pageNo_), y: 0), animated: true)
+        pageNo = pageNo_ - 1
+        print("--\(pageNo)--")
+        pageControl.currentPage = pageNo
+  
+    }
+    @objc func handleTap(sender : UIGestureRecognizer) {
+        let point = sender.location(in: scrollView)
+        let pageIndex = Int(point.x/self.frameW) - 1
+        delegate?.circleViewClick(clickAtIndex: pageIndex)
+    }
     func setImageNames(images:Array<Any>) {
         
         imageNames = images
         
         if imageNames.count > 0 {
-            
-            let first = imageNames.first
+
             let last = imageNames.last
             imageNames.insert(last! , at: 0)
-            imageNames.append(first!)
 
             pageControl.frame = CGRect(x: (self.frameW-150)/2.0, y: self.frameH-50, width: 150, height: 50)
             self.addSubview(pageControl)
-            pageControl.numberOfPages = imageNames.count-2
+            pageControl.numberOfPages = imageNames.count-1
             pageControl.currentPageIndicatorTintColor = UIColor.blue
             pageControl.pageIndicatorTintColor = UIColor.black
             pageControl.addTarget(self, action: #selector(pageChange), for: .valueChanged)
-            
+
+            let lastTitle = titles.last
+            titles.insert(lastTitle! , at: 0)
+
             for i in 0..<imageNames.count {
                 let imageView = UIImageView(frame: CGRect(x: CGFloat(i)*self.frameW, y: 0.0, width: self.frameW, height: self.frameH))
                 scrollView.addSubview(imageView)
@@ -62,6 +100,12 @@ class ZJCircleView: UIView {
                     imageView.image = sub as? UIImage
                 }
  
+                //title
+                let titleLabel = UILabel(frame: CGRect(x: CGFloat(i)*self.frameW, y: self.frameH - 30, width: self.frameW, height: 30))
+                titleLabel.textAlignment = .right
+                scrollView.addSubview(titleLabel)
+                titleLabel.text = titles[i]
+                
             }
             scrollView.contentSize = CGSize(width: self.frameW * CGFloat(imageNames.count), height: 0);
             scrollView.setContentOffset(CGPoint(x: self.frameW, y: 0), animated: false)
@@ -82,27 +126,18 @@ extension ZJCircleView: UIScrollViewDelegate {
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         
-        var pageNo = scrollView.contentOffset.x/self.frameW
-  
-        
-        if Int(pageNo) == imageNames.count - 1 {
-            scrollView.setContentOffset(CGPoint(x: self.frameW, y: 0), animated: false)
-            
-            pageNo = 0
-
-        }else if Int(pageNo) == 0 {
-             scrollView.setContentOffset(CGPoint(x: self.frameW * CGFloat(imageNames.count - 2), y: 0), animated: false)
-            
-            pageNo = CGFloat(imageNames.count - 3)
-
-        }else{
-            
+        pageNo = Int(scrollView.contentOffset.x/self.frameW)
+        if pageNo == imageNames.count - 1  {
+            scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: false)
             pageNo -= 1
-
+        }else if pageNo == 0 {
+            scrollView.setContentOffset(CGPoint(x: self.frameW * CGFloat(imageNames.count - 1), y: 0), animated: false)
+            pageNo = imageNames.count - 2
+        }else{
+            pageNo -= 1
         }
-
-        pageControl.currentPage = Int(pageNo)
-        
+        pageControl.currentPage = pageNo
+        print("--\(pageNo)--")
     }
     
 }
